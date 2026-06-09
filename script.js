11779c70970e69d91b8d4614f9cca059230bd8c7
adamswrcznsk-afk <adamswrcznsk@gmail.com>
2026-06-09 22:25:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,30 @@ setInterval(()=>{let d=end-new Date();
 if(d<0){document.getElementById("timer").innerText="Promocja zakończona";return;}
 let h=Math.floor(d/3600000),m=Math.floor((d%3600000)/60000),s=Math.floor((d%60000)/1000);
 document.getElementById("timer").innerText=`${h}h ${m}m ${s}s`;},1000);
+document
+.getElementById("orderForm")
+.addEventListener("submit", async function(e){
+
+e.preventDefault();
+
+const formData = {
+  imie: this.imie.value,
+  email: this.email.value,
+  telefon: this.telefon.value,
+  adres: this.adres.value,
+  miasto: this.miasto.value,
+  ilosc: this.ilosc.value
+};
+
+await fetch(
+"https://script.google.com/macros/s/AKfycbyVZBv0byXiDq6QX1g5PgAYXQMA4fjyRxc2RE1ueZCqrYjh3hlNAgE5Qja6UZI_vLpF/exec",
+{
+method:"POST",
+body:JSON.stringify(formData)
+});
+
+alert("Dziękujemy za zamówienie!");
+
+this.reset();
+
+});
